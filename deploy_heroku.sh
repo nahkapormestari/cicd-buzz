52e019487d2f34fd3d0834745faf08304a773346
Juhpek <juho.pekki@gmail.com>
2018-04-12 15:15:25 +0300

    Delete deploy_heroku.sh

--- deploy_heroku.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
-docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
