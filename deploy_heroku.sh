724a8fb482f30f05d6aa91cdcfde8da75d4d3b85
Juho Pekki <juho.pekki@jamk.fi>
2018-04-12 14:58:12 +0300

    asd

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
-docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
